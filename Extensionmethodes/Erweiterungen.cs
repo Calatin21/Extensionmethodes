8fa63a8b43ac93d4e47223ff6e272a608ef06374
Calatin21 <oshaughnessy@hotmail.de>
2023-05-05 14:30:07 +0200

    Add project files.

---
+++ Extensionmethodes/Erweiterungen.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace Extensionmethodes {
+    internal static class Erweiterungen {
+        public static void BuchstabenZaehlen(this string s) {
+            int ergebnis = 0;
+            foreach (char item in s) {
+                if (char.IsLetter(item)) {
+                    ergebnis++;
+                }
+            }
+            Console.WriteLine(ergebnis);
+        }
+        public static void WoerterZaehlen(this string s) {
+            int ergebnis = 1;
+            for (int i = 0; i < s.Length; i++) {
+                if (char.IsWhiteSpace(s[i])) {
+                    if (i - 1 >= 0 && i + 1 < s.Length) {
+                        if (char.IsLetter(s[i - 1]) && char.IsLetter(s[i + 1])) {
+                            ergebnis++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(ergebnis);
+            //string[] woerter = s.Split(new[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            //Console.WriteLine(woerter.Length);
+        }
+        public static void AddCopyright(this string s, int abstand) {
+            //string[] woerter = s.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            //foreach (string item in woerter) {
+            //    Console.WriteLine(item);
+            //}
+            StringBuilder sb = new StringBuilder();
+            sb.Append(s);
+            Console.WriteLine(sb);
+            //for(int i = 0; i  ; i++) { }
+            //for (int i = 0; i < woerter.Length; i++) {
+            //    if (i % (abstand - 1) == 0) {
+            //        woerter[i] += " BITLC Neuss";
+            //    }
+            //}
+            //s = "";
+            //foreach (string item in woerter) {
+            //    s += item + " ";
+            //}
+            //Console.WriteLine(s);
+            //int ergebnis = 1;
+            //string cache = s;
+            //List<int> zahlen = new();
+            //StringBuilder sb = new();
+            //for (int i = 0; i < s.Length; i++) {
+            //    if (char.IsWhiteSpace(s[i])) {
+            //        if (i - 1 >= 0 && i + 1 < s.Length) {
+            //            if (char.IsLetter(s[i - 1]) && char.IsLetter(s[i + 1])) {
+            //                ergebnis++;
+            //            }
+            //        }
+            //    }
+            //    if (ergebnis % (abstand + 1) == 0 || i == 0) {
+            //        zahlen.Add(i);
+            //    }
+            //}
+
+            //foreach (int item in zahlen) {
+            //    cache = cache.Insert(zahlen[item], "BITLC Neuss ");
+            //}
+            //s = cache;
+            //Console.WriteLine(s);
+        }
+    }
+}
---
+++ Extensionmethodes/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Extensionmethodes {
+    internal class Program {
+        static void Main(string[] args) {
+            string wort = " h h h ";
+            wort.BuchstabenZaehlen();
+            wort.WoerterZaehlen();
+            string gedicht = "In visions of the dark night\n\n In visions of the dark night I have dreamed of joy departed, But a waking dream of life and light Hath left me broken-hearted. \n\n Ah! what is not a dream by day To him whose eyes are cast On things around him with a ray Turned back upon the past? \n\n That holy dream -that holy dream, While all the world were chiding, Hath cheered me as a lovely beam A lonely spirit guiding.\n\n What though that light, thro' storm and night, So trembled from afar, What could there be more purely bright In Truth's day-star. \n\n\n\n(Edgar Allan Po 1809-1849, US-amerikanischer Schriftsteller - A dream)";
+            Console.WriteLine(gedicht);
+            gedicht.BuchstabenZaehlen();
+            gedicht.WoerterZaehlen();
+            Console.WriteLine("");
+            gedicht.AddCopyright(15);
+        }
+    }
+}
